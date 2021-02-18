f39b5633423b06dae49d24bfc83763b04f48d110
Joseph Cordell <joseph@jcordell.com>
2021-02-17 16:44:30 -0800

    Rewrote js functions for efficiency

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -26,7 +26,6 @@ generateBtn.addEventListener("click", writePassword);
 
 
 function generatePassword(length, charset) {
-
   var retVal = "";
 
   for (var i = 0, n = charset.length; i < length; ++i) {
@@ -36,63 +35,38 @@ function generatePassword(length, charset) {
 }
 
 function createLength(newLength) {
-
   newLength = prompt(txt, `8`);
-  
   if (newLength < 8 || newLength > 128) {
     txt = `Try again, you must choose between 8 and 128.`
     createLength(newLength)
   }
     return newLength
-
 }
 
 function lowercase(charset) {
-    txt = `Do you want lower case characters? (yes or no)`
-    lower = prompt(txt, `yes`)
-    lower = lower.toLowerCase()
-    console.log(lower)
-    if (lower === 'yes') {
-        charset += `abcdefghijklmnopqrstuvwxyz`
-    } else{
-      charset = charset
-    }
+    if (confirm(`Do you want lower case characters?`)){
+      charset += `abcdefghijklmnopqrstuvwxyz`
+    } 
     return charset
 }
 
 function uppercase(charset) {
-  txt = `Do you want upper case characters? (yes or no)`
-  upper = prompt(txt, `yes`)
-  upper = upper.toLowerCase()
-  console.log(upper)
-  if (upper == 'yes') {
+  if (confirm(`Do you want upper case characters?`)){
     charset += `ABCDEFGHIJKLMNOPQRSTUVWXYZ`
-  } else{
-    charset = charset
-  }
+  } 
   return charset
 }
 
 function numbers(charset) {
-  txt = `Do you want numbers? (yes or no)`
-  numberino = prompt(txt, `yes`)
-  numberino = numberino.toLowerCase()
-  if (numberino == 'yes') {
+  if (confirm(`Do you want numbers?`)){
     charset += `0123456789`
-  } else{
-    charset = charset
-  }
+  } 
   return charset
 }
 
 function symboll(charset) {
-  txt = `Do you want symbols? (yes or no)`
-  symbolino = prompt(txt, `yes`) 
-  symbolino = symbolino.toLowerCase()
-  if (symbolino == 'yes') {
+  if (confirm(`Do you want symbols?`)){
     charset += `!@#$%^&*`
-  } else{
-    charset = charset
-  }
+  } 
   return charset
 }
